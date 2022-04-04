ef5252d4654ef4f425a6eab0bd35d6722b3d669a
andora814 <smith.doran@gmail.com>
2022-04-04 10:18:57 -0400

    created sign up component, form input component, button component

--- crwn-clothing/src/routes/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -1,15 +1,31 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils.js'
+// import { useEffect } from 'react';
+// import { getRedirectResult } from '@firebase/auth';
+import { signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect, auth } from '../../utils/firebase/firebase.utils.js'
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component.jsx';
+
 
 const SignIn = () => {
+
+  // useEffect(async() => {
+  //   const response = await getRedirectResult(auth)
+  //   if(response) {
+  //     const userDocRef = await createUserDocumentFromAuth(response.user)
+  //   }
+  // }, [])
+
   const logGoogleUser = async () => {
     const {user} = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user)
   }
 
+
+
   return (
     <div> 
       <h1>Sign In</h1>
       <button onClick={logGoogleUser}>Sign in with Google</button>
+      {/* <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button> */}
+      <SignUpForm />
     </div>
   )
 }
